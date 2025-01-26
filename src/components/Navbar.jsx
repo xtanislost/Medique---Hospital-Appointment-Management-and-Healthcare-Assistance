5f04812d575001e1494deabc6d9d27fab36ce0ed
xtanislost <150476764+xtanislost@users.noreply.github.com>
2025-01-26 20:20:19 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import {assets} from '../assets/assets'
+import { NavLink, useNavigate } from 'react-router-dom'
+
+const Navbar = () => { 
+
+    const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] = useState(false)
+    const [token,setToken] = useState(false)
+
+
+  return (
+    <div className = 'flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400' >
+        <img className = 'w-44 cursor-pointer' src={assets.logo} alt='' />
+        <ul className= 'hidden md:flex items-start gap-5 font-medium'>
+            <NavLink to='/'>
+                <li className='py-1'>HOME</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'hidden/>
+            </NavLink>
+
+            <NavLink to='/about'>
+                <li className='py-1'>ABOUT</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'hidden/>
+            </NavLink>
+
+            <NavLink to='/doctors'>
+                <li className='py-1'>DOCTORS</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'hidden/>
+            </NavLink>
+
+            <NavLink to='/contact'>
+                <li className='py-1'>CONTACT</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'hidden/>
+            </NavLink>
+            
+
+
+        </ul>
+        <div className='flex item-center gap-4'>
+            {
+                token 
+                ? <div>
+                    <img src={assets.profile_pic} alt="" />
+                    <img src={assets.dropdown_icon} alt="" />
+                </div>
+                : <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+
+            }
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar 
