201049a1b1748741cf403e6c849cc352129bbc9b
xtanislost <150476764+xtanislost@users.noreply.github.com>
2025-03-07 17:10:37 +0530

    Creatting admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,44 @@
 import React, { useContext } from 'react'
 import Login from './pages/login';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllApointments from './pages/Admin/AllApointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 
 const App = () => {
 
-  return  (
-    <div>
-      <Login/>
+  const {aToken} = useContext(AdminContext)
+
+  return aToken ? (
+    <div className='bg-[#F8F9FD]'>
+
+      <ToastContainer />
+      <Navbar/>
+      <div className='flex items-start' >
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllApointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+        </Routes>
+
+      </div>
 
     </div>
-
-
+  ) : (
+    <>
+      <Login/>
+      <ToastContainer />
+    
+    </>
   )
 }
 
