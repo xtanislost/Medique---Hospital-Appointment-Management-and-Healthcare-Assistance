e63a249614dac0b2a417f26ef8169f86e9f5c6e6
bprerna28 <bishtprerna2813@gmail.com>
2025-03-16 20:19:22 +0530

    pulling the changes

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -30,12 +30,33 @@ const AdminContextProvider = (props) => {
     }
   };
 
+  const changeAvailability = async(docId) =>{
+
+    try {
+
+      const {data}= await axios.post(backendUrl+'/api/admin/change-availability',{docId},{headers:{aToken}})
+      if (data.sucess){
+        toast.success(data.message)
+        getAllDoctors()
+      }else{
+        toast.error(error.message)
+      }
+
+      
+      
+    } catch (error) {
+      toast.error(error.message);
+      
+    }
+  }
+
   const value = {
     aToken,
     setAToken,
     backendUrl,
     doctors,
     getAllDoctors,
+    changeAvailability,
   };
 
   return (
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
+import userRouter from './routes/userRoute.js'
 
 
 
@@ -19,6 +20,8 @@ app.use(cors())
 
 //api endpoints
 app.use('/api/admin', adminRouter)
+app.use('api/user', userRouter)
+
 
 
 app.get('/',(req,res)=>{
