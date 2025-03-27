f7449a6f2d7ddcd3329cadf65354b323cd65d6d4
bprerna28 <bishtprerna2813@gmail.com>
2025-03-27 17:41:00 +0530

    doctor panel logout

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,10 +2,13 @@ import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import {AdminContext} from '../context/AdminContext'
 import {useNavigate} from 'react-router-dom'
+import { DoctorContext } from '../context/DoctorContext'
 
 const Navbar = () => {
 
     const {aToken,setAToken} = useContext(AdminContext)
+    const {dToken,setDtoken}= useContext(DoctorContext)
+    
 
     const navigate = useNavigate()
 
@@ -13,6 +16,8 @@ const Navbar = () => {
         navigate('/')
         aToken && setAToken('')
         aToken && localStorage.removeItem('aToken')
+        dToken && setDtoken('')
+        dToken && localStorage.removeItem('dToken')
 
     }
   return (
