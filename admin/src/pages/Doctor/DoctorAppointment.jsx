69123380891a17d365e95630712dee8c4dbc5189
xtanislost <150476764+xtanislost@users.noreply.github.com>
2025-03-27 10:12:22 +0530

    doctor panel dashboard and others

--- admin/src/pages/Doctor/DoctorAppointment.jsx
+++ admin/src/pages/Doctor/DoctorAppointment.jsx
@@ -30,7 +30,7 @@ const DoctorAppointments = () => {
           <p>Fees</p>
           <p>Action</p>
         </div>
-        {appointments.map((item, index) => (
+        {appointments.reverse().map((item, index) => (
           <div className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50' key={index}>
             <p className='max-sm:hidden'>{index}</p>
             <div className='flex items-center gap-2'>
@@ -54,7 +54,7 @@ const DoctorAppointments = () => {
                 </div>
             }
           </div>
-        ))}
+        ))} 
       </div>
 
     </div>
