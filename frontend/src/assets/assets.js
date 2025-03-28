8eab86b51ddf089d6a09a807818ef654bac48f2c
bprerna28 <bishtprerna2813@gmail.com>
2025-03-28 23:35:03 +0530

    added reviews and blog UI

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -36,6 +36,9 @@ import General_physician from './General_physician.svg'
 import Gynecologist from './Gynecologist.svg'
 import Neurologist from './Neurologist.svg'
 import Pediatricians from './Pediatricians.svg'
+import healthy from './healthy.jpeg'
+import checkup from './checkup.jpg'
+import food from './food.jpeg'
 
 
 export const assets = {
@@ -55,7 +58,10 @@ export const assets = {
     dropdown_icon,
     upload_icon,
     stripe_logo,
-    razorpay_logo
+    razorpay_logo,
+    food,
+    healthy,
+    checkup,
 }
 
 export const specialityData = [
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,6 +36,14 @@ const Navbar = () => {
           <li className='py-1'>CONTACT</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
+        <NavLink to='/blog' >
+          <li className='py-1'>BLOG</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+        </NavLink>
+        <NavLink to='/reviews' >
+          <li className='py-1'> REVIEWS</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+        </NavLink>
       </ul>
 
       <div className='flex items-center gap-4 '>
